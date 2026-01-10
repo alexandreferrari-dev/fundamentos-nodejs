2bab167817718ad4f4e2fceaf13e5ddf511af810
Alexandre Ferrari <ale.vertigem@gmail.com>
2026-01-09 21:41:55 -0300

    corpo-da-requisicao-em-json-stream-buffers

--- src/server.js
+++ src/server.js
@@ -1,17 +1,32 @@
 import http from 'node:http';
 const users = [];
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    // percorre toda a stream
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
   if (method === 'GET' && url === '/users') {
     return res
       .setHeader('Content-Type', 'application/json')
       .end(JSON.stringify(users));
   }
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body;
     users.push({
       id: 1,
-      name: 'John Doe',
-      email: 'johndoe@eample.com',
+      name,
+      email,
     });
     return res.writeHead(201).end();
   }
