9dc80daf37008ec5c549a0751a49b473e7ed6072
Alexandre Ferrari <ale.vertigem@gmail.com>
2026-01-30 18:39:19 -0300

    search

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
 import { routes } from './routes.js';
+import { extractQueryParams } from './utils/extract-query-params.js';
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
@@ -13,7 +14,12 @@ const server = http.createServer(async (req, res) => {
 
   if (routes) {
     const routeParams = req.url.match(route.path);
-    req.params = { ...routeParams.groups };
+
+    //console.log(extractQueryParams(routeParams.groups.query));
+    const { query, ...params } = routeParams.groups;
+
+    req.params = params;
+    req.query = query ? extractQueryParams(query) : {};
 
     return route.handler(req, res);
   }
